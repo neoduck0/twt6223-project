36c8d80135ab033ed5714361a5c3afdfc882859a
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2026-06-28 04:33:04 +0800

    feat: add the is_admin field to the users table in database

--- db/init.sql
+++ db/init.sql
@@ -7,6 +7,7 @@ CREATE TABLE users (
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
